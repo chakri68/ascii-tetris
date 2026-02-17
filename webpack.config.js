32fcb9e19b72b5ccdd12bace4ffe51be03a57ba6
chakri <99131274+chakri68@users.noreply.github.com>
2026-02-17 22:47:12 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { buildEntries, buildPlugins } = require("./scripts/build-path.js");
+
+async function buildConfig() {
+  const plugins = await buildPlugins();
+  const entries = buildEntries();
+
+  return {
+    mode: "none",
+    entry: { ...entries },
+    output: {
+      globalObject: "self",
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+      contentBase: "./dist",
+      hot: true,
+    },
+    optimization: {
+      runtimeChunk: "single",
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+    plugins: [
+      ...plugins,
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: "./src/public", to: "assets", noErrorOnMissing: true },
+        ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
+    ],
+    devtool: "eval-source-map",
+    devServer: {
+      static: path.join(__dirname, "dist"),
+      hot: true,
+    },
+    resolve: {
+      extensions: ["*", ".ts", ".js"],
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(s(a|c)ss)$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.ts$/,
+          exclude: /node_module/,
+          use: "ts-loader",
+        },
+      ],
+    },
+  };
+}
+
+module.exports = buildConfig();
